68aa401de22487a82265d04fcf3729f9520c824a
ChrisAppier <72580749+ChrisAppier@users.noreply.github.com>
2020-10-20 18:43:17 -0500

    Input/Output Update
    Changed the input to read from a file, rather than user input.  Added a loop to perform calculations for each set of data points from the input files.  Added a check to ensure input files are the same size.  Outputs values are now saved to a file.

--- LimeSodaAshModel.m
+++ LimeSodaAshModel.m
@@ -2,115 +2,111 @@
 %calcium, magnesium, barium, and strontium hardness in water. Strontium
 %removal is through substitution with calcium.
 
-prompt = 'Enter calcium concentration (mol/L)\n';
-Ca_start = input(prompt);
+%Loading the input data files as mol/L
 
-prompt = 'Enter magnesium concentration (mol/L)\n';
-Mg_start = input(prompt);
+load('Ca_input')
+load('Mg_input')
+load('Ba_input')
+load('Sr_input')
+load('Alk_input')
+load('inefficiency')
 
-prompt = 'Enter barium concentration (mol/L)\n';
-Ba_start = input(prompt);
+%Defining constants, these still need to be filled in with real #s
 
-prompt = 'Enter strontium concentration (mol/L)\n';
-Sr_start = input(prompt);
+Ca_end = 0.01;
+Mg_end = 0.01;
+Ba_end = 0.01;
+Sr_end = 0.01;
+s = 3;
+Ksp = 10 * 10^(-8);
 
-prompt = 'Enter the alkalinity (mol/L)\n';
-Alk = input(prompt);
+%Checking the number of data points for each input to ensure they are equal and
+%setting the number of loops for the model equal to that size or ending the
+%program
 
-prompt = 'Enter desired calcium concentration (mol/L)\n';
-Ca_end = input(prompt);
+if isequal(height(Ca_input),height(Mg_input),height(Ba_input),height(Sr_input),height(inefficiency))
+    
+    n = height(Ba_input);
+    
+else
 
-prompt = 'Enter desired magnesium concentration (mol/L)\n';
-Mg_end = input(prompt);
+    disp('Input files contain unequal number of data points. The program will not run. Check the data and try again')      
+    return
+    
+end
 
-prompt = 'Enter desired barium concentration (mol/L)\n';
-Ba_end = input(prompt);
+%Main loop
 
-prompt = 'Enter desired strontium concentration (mol/L)\n';
-Sr_end = input(prompt);
+for i = 1:n
 
 %Calculates change in pollutant concentrations
 
-Ca = Ca_start - Ca_end;
+    Ca = Ca_input(i) - Ca_end;
 
-Mg = Mg_start - Mg_end;
+    Mg = Mg_input(i) - Mg_end;
 
-Ba = Ba_start - Ba_end;
+    Ba = Ba_input(i) - Ba_end;
 
-Sr = Sr_start - Sr_end;
-
-%Declaires Sr to Ca removal ratio (Ca removed per Sr) and CaCO3 Ksp
-
-s = 3;
-
-Ksp = 3.36 * 10^(-9);
+    Sr = Sr_input(i) - Sr_end;
 
 %Checking if enough Ca is present to remove Sr and adds soda ash if not.
 %First if statement is for all calcium carbonate hardness, second if
 %statement is for combination of carbonate and non-carbonate calcium
 %hardness
 
-if 2 * Ca < s * Sr && 2 * Ca <= Alk
+    if 2 * Ca < s * Sr && 2 * Ca <= Alk_input(i)
     
-    Ca_add = (s * Sr) - (2 * Ca);
+        Ca_add = (s * Sr) - (2 * Ca);
     
-    Ca = Ca + Ca_add;
+        Ca = Ca + Ca_add;
     
-end
+    end
 
-if 2 * Ca < s * Sr && 2 * Ca > Alk
+    if 2 * Ca < s * Sr && 2 * Ca > Alk_input(i)
     
-    Ca_add = (s * Sr) - Alk - (Ca - (0.5 * Alk));
+        Ca_add = (s * Sr) - Alk_input(i) - (Ca - (0.5 * Alk_input(i));
     
-    Ca = Ca + Ca_add;
+        Ca = Ca + Ca_add;
     
-end
+    end
 
 %Calculates carbonate hardness (CCa and CMg) and non-carbonate hardness
 %(NCCa and NCMg). Ba and Sr behave similar to Ca and are all combined under
 %the CCa and NCCa variables
 
-if 2 * (Ca + Ba + Sr + Mg) <= Alk
+    if 2 * (Ca + Ba + Sr + Mg) <= Alk_input(i)
     
-    CCa = Ca + Ba + Sr;
-    NCCa = 0;
-    CMg = Mg;
-    NCMg = 0;
+        CCa = Ca + Ba + Sr;
+        NCCa = 0;
+        CMg = Mg;
+        NCMg = 0;
 
-elseif 2 * (Ca + Ba + Sr + Mg) > Alk && 2 * (Ca + Ba + Sr) <= Alk
+    elseif 2 * (Ca + Ba + Sr + Mg) > Alk_input(i) && 2 * (Ca + Ba + Sr) <= Alk_input(i)
     
-    CCa = Ca + Ba + Sr;
-    NCCa = 0;
-    CMg = (Alk - 2 * (Ca + Ba + Sr)) / 2;
-    NCMg = Mg - CMg;
+        CCa = Ca + Ba + Sr;
+        NCCa = 0;
+        CMg = (Alk_input(i) - 2 * (Ca + Ba + Sr)) / 2;
+        NCMg = Mg - CMg;
     
-elseif 2 * (Ca + Ba + Sr + Mg) > Alk && 2 * (Ca + Ba + Sr) > Alk
+    elseif 2 * (Ca + Ba + Sr + Mg) > Alk_input(i) && 2 * (Ca + Ba + Sr) > Alk_input(i)
     
-    CCa = Alk;
-    NCCa = Ca + Ba + Sr - (0.5 * Alk);
-    CMg = 0;
-    NCMg = Mg;
+        CCa = Alk_input(i);
+        NCCa = Ca + Ba + Sr - (0.5 * Alk_input(i));
+        CMg = 0;
+        NCMg = Mg;
     
-end
+    end
 
 %Calculates the amount of lime and soda ash required
 
-Lime = CCa + 2*CMg + NCMg + (Ksp / Ca_end);
+    Lime(i) = CCa + 2*CMg + NCMg + (Ksp / Ca_end);
 
-Soda = NCCa + NCMg + Ca_add;
+    Soda(i) = NCCa + NCMg + Ca_add;
+    
+end
 
-%Debug Section
-%disp('CCa =');
-%disp(CCa);
-%disp('NCCa =');
-%disp(NCCa);
-%disp('CMg');
-%disp(CMg);
-%disp('NCMg =');
-%disp(NCMg);
-
-%Additions required output
-disp('Lime =');
-disp(Lime);
-disp('Soda Ash =')
-disp(Soda);
+%Transposes the columns to rows and writes the lime and soda ash additions to a text file
+Lime = transpose(Lime);
+Soda = transpose(Soda);
+writematrix(Lime,'Lime_Addition.txt');
+writematrix(Soda, 'SodaAsh_Addition.txt');
